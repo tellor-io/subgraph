7b5839e75f9aeb573daa38f4f61db6efd059e74d
skuhlmann <samkuhlmann@gmail.com>
2020-05-20 15:36:01 -0600

    gets basic entity structure

--- generated/schema.ts
+++ generated/schema.ts
@@ -12,6 +12,322 @@ import {
   BigDecimal
 } from "@graphprotocol/graph-ts";
 
+export class Request extends Entity {
+  constructor(id: string) {
+    super();
+    this.set("id", Value.fromString(id));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id !== null, "Cannot save Request entity without an ID");
+    assert(
+      id.kind == ValueKind.STRING,
+      "Cannot save Request entity with non-string ID. " +
+        'Considering using .toHex() to convert the "id" to a string.'
+    );
+    store.set("Request", id.toString(), this);
+  }
+
+  static load(id: string): Request | null {
+    return store.get("Request", id) as Request | null;
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get timestamp(): BigInt {
+    let value = this.get("timestamp");
+    return value.toBigInt();
+  }
+
+  set timestamp(value: BigInt) {
+    this.set("timestamp", Value.fromBigInt(value));
+  }
+
+  get requestId(): BigInt {
+    let value = this.get("requestId");
+    return value.toBigInt();
+  }
+
+  set requestId(value: BigInt) {
+    this.set("requestId", Value.fromBigInt(value));
+  }
+
+  get sender(): Bytes {
+    let value = this.get("sender");
+    return value.toBytes();
+  }
+
+  set sender(value: Bytes) {
+    this.set("sender", Value.fromBytes(value));
+  }
+
+  get query(): string {
+    let value = this.get("query");
+    return value.toString();
+  }
+
+  set query(value: string) {
+    this.set("query", Value.fromString(value));
+  }
+
+  get querySymbol(): string {
+    let value = this.get("querySymbol");
+    return value.toString();
+  }
+
+  set querySymbol(value: string) {
+    this.set("querySymbol", Value.fromString(value));
+  }
+
+  get granularity(): BigInt {
+    let value = this.get("granularity");
+    return value.toBigInt();
+  }
+
+  set granularity(value: BigInt) {
+    this.set("granularity", Value.fromBigInt(value));
+  }
+
+  get totalTips(): BigInt {
+    let value = this.get("totalTips");
+    return value.toBigInt();
+  }
+
+  set totalTips(value: BigInt) {
+    this.set("totalTips", Value.fromBigInt(value));
+  }
+
+  get miningEvents(): Array<string> | null {
+    let value = this.get("miningEvents");
+    if (value === null) {
+      return null;
+    } else {
+      return value.toStringArray();
+    }
+  }
+
+  set miningEvents(value: Array<string> | null) {
+    if (value === null) {
+      this.unset("miningEvents");
+    } else {
+      this.set("miningEvents", Value.fromStringArray(value as Array<string>));
+    }
+  }
+}
+
+export class MiningEvent extends Entity {
+  constructor(id: string) {
+    super();
+    this.set("id", Value.fromString(id));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id !== null, "Cannot save MiningEvent entity without an ID");
+    assert(
+      id.kind == ValueKind.STRING,
+      "Cannot save MiningEvent entity with non-string ID. " +
+        'Considering using .toHex() to convert the "id" to a string.'
+    );
+    store.set("MiningEvent", id.toString(), this);
+  }
+
+  static load(id: string): MiningEvent | null {
+    return store.get("MiningEvent", id) as MiningEvent | null;
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get timestamp(): BigInt {
+    let value = this.get("timestamp");
+    return value.toBigInt();
+  }
+
+  set timestamp(value: BigInt) {
+    this.set("timestamp", Value.fromBigInt(value));
+  }
+
+  get requestId(): BigInt {
+    let value = this.get("requestId");
+    return value.toBigInt();
+  }
+
+  set requestId(value: BigInt) {
+    this.set("requestId", Value.fromBigInt(value));
+  }
+
+  get request(): string {
+    let value = this.get("request");
+    return value.toString();
+  }
+
+  set request(value: string) {
+    this.set("request", Value.fromString(value));
+  }
+
+  get time(): BigInt {
+    let value = this.get("time");
+    return value.toBigInt();
+  }
+
+  set time(value: BigInt) {
+    this.set("time", Value.fromBigInt(value));
+  }
+
+  get minedValue(): BigInt {
+    let value = this.get("minedValue");
+    return value.toBigInt();
+  }
+
+  set minedValue(value: BigInt) {
+    this.set("minedValue", Value.fromBigInt(value));
+  }
+
+  get totalTips(): BigInt {
+    let value = this.get("totalTips");
+    return value.toBigInt();
+  }
+
+  set totalTips(value: BigInt) {
+    this.set("totalTips", Value.fromBigInt(value));
+  }
+
+  get currentChallenge(): Bytes {
+    let value = this.get("currentChallenge");
+    return value.toBytes();
+  }
+
+  set currentChallenge(value: Bytes) {
+    this.set("currentChallenge", Value.fromBytes(value));
+  }
+
+  get minerValues(): Array<string> | null {
+    let value = this.get("minerValues");
+    if (value === null) {
+      return null;
+    } else {
+      return value.toStringArray();
+    }
+  }
+
+  set minerValues(value: Array<string> | null) {
+    if (value === null) {
+      this.unset("minerValues");
+    } else {
+      this.set("minerValues", Value.fromStringArray(value as Array<string>));
+    }
+  }
+}
+
+export class MinerValue extends Entity {
+  constructor(id: string) {
+    super();
+    this.set("id", Value.fromString(id));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id !== null, "Cannot save MinerValue entity without an ID");
+    assert(
+      id.kind == ValueKind.STRING,
+      "Cannot save MinerValue entity with non-string ID. " +
+        'Considering using .toHex() to convert the "id" to a string.'
+    );
+    store.set("MinerValue", id.toString(), this);
+  }
+
+  static load(id: string): MinerValue | null {
+    return store.get("MinerValue", id) as MinerValue | null;
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get timestamp(): BigInt {
+    let value = this.get("timestamp");
+    return value.toBigInt();
+  }
+
+  set timestamp(value: BigInt) {
+    this.set("timestamp", Value.fromBigInt(value));
+  }
+
+  get requestId(): BigInt {
+    let value = this.get("requestId");
+    return value.toBigInt();
+  }
+
+  set requestId(value: BigInt) {
+    this.set("requestId", Value.fromBigInt(value));
+  }
+
+  get currentChallenge(): Bytes {
+    let value = this.get("currentChallenge");
+    return value.toBytes();
+  }
+
+  set currentChallenge(value: Bytes) {
+    this.set("currentChallenge", Value.fromBytes(value));
+  }
+
+  get miningEvent(): string {
+    let value = this.get("miningEvent");
+    return value.toString();
+  }
+
+  set miningEvent(value: string) {
+    this.set("miningEvent", Value.fromString(value));
+  }
+
+  get miner(): Bytes {
+    let value = this.get("miner");
+    return value.toBytes();
+  }
+
+  set miner(value: Bytes) {
+    this.set("miner", Value.fromBytes(value));
+  }
+
+  get nonce(): string {
+    let value = this.get("nonce");
+    return value.toString();
+  }
+
+  set nonce(value: string) {
+    this.set("nonce", Value.fromString(value));
+  }
+
+  get value(): BigInt {
+    let value = this.get("value");
+    return value.toBigInt();
+  }
+
+  set value(value: BigInt) {
+    this.set("value", Value.fromBigInt(value));
+  }
+}
+
 export class Dispute extends Entity {
   constructor(id: string) {
     super();
@@ -42,15 +358,6 @@ export class Dispute extends Entity {
     this.set("id", Value.fromString(value));
   }
 
-  get count(): BigInt {
-    let value = this.get("count");
-    return value.toBigInt();
-  }
-
-  set count(value: BigInt) {
-    this.set("count", Value.fromBigInt(value));
-  }
-
   get miner(): Bytes {
     let value = this.get("miner");
     return value.toBytes();
@@ -76,6 +383,159 @@ export class Dispute extends Entity {
 
   set requestId(value: BigInt) {
     this.set("requestId", Value.fromBigInt(value));
+  }
+
+  get result(): BigInt | null {
+    let value = this.get("result");
+    if (value === null) {
+      return null;
+    } else {
+      return value.toBigInt();
+    }
+  }
+
+  set result(value: BigInt | null) {
+    if (value === null) {
+      this.unset("result");
+    } else {
+      this.set("result", Value.fromBigInt(value as BigInt));
+    }
+  }
+
+  get reportedMiner(): Bytes | null {
+    let value = this.get("reportedMiner");
+    if (value === null) {
+      return null;
+    } else {
+      return value.toBytes();
+    }
+  }
+
+  set reportedMiner(value: Bytes | null) {
+    if (value === null) {
+      this.unset("reportedMiner");
+    } else {
+      this.set("reportedMiner", Value.fromBytes(value as Bytes));
+    }
+  }
+
+  get reportingParty(): Bytes | null {
+    let value = this.get("reportingParty");
+    if (value === null) {
+      return null;
+    } else {
+      return value.toBytes();
+    }
+  }
+
+  set reportingParty(value: Bytes | null) {
+    if (value === null) {
+      this.unset("reportingParty");
+    } else {
+      this.set("reportingParty", Value.fromBytes(value as Bytes));
+    }
+  }
+
+  get active(): boolean {
+    let value = this.get("active");
+    return value.toBoolean();
+  }
+
+  set active(value: boolean) {
+    this.set("active", Value.fromBoolean(value));
+  }
+
+  get timestamp(): BigInt {
+    let value = this.get("timestamp");
+    return value.toBigInt();
+  }
+
+  set timestamp(value: BigInt) {
+    this.set("timestamp", Value.fromBigInt(value));
+  }
+
+  get votes(): Array<string> | null {
+    let value = this.get("votes");
+    if (value === null) {
+      return null;
+    } else {
+      return value.toStringArray();
+    }
+  }
+
+  set votes(value: Array<string> | null) {
+    if (value === null) {
+      this.unset("votes");
+    } else {
+      this.set("votes", Value.fromStringArray(value as Array<string>));
+    }
+  }
+}
+
+export class Vote extends Entity {
+  constructor(id: string) {
+    super();
+    this.set("id", Value.fromString(id));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id !== null, "Cannot save Vote entity without an ID");
+    assert(
+      id.kind == ValueKind.STRING,
+      "Cannot save Vote entity with non-string ID. " +
+        'Considering using .toHex() to convert the "id" to a string.'
+    );
+    store.set("Vote", id.toString(), this);
+  }
+
+  static load(id: string): Vote | null {
+    return store.get("Vote", id) as Vote | null;
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get dispute(): string {
+    let value = this.get("dispute");
+    return value.toString();
+  }
+
+  set dispute(value: string) {
+    this.set("dispute", Value.fromString(value));
+  }
+
+  get disputeId(): BigInt {
+    let value = this.get("disputeId");
+    return value.toBigInt();
+  }
+
+  set disputeId(value: BigInt) {
+    this.set("disputeId", Value.fromBigInt(value));
+  }
+
+  get position(): boolean {
+    let value = this.get("position");
+    return value.toBoolean();
+  }
+
+  set position(value: boolean) {
+    this.set("position", Value.fromBoolean(value));
+  }
+
+  get voter(): Bytes {
+    let value = this.get("voter");
+    return value.toBytes();
+  }
+
+  set voter(value: Bytes) {
+    this.set("voter", Value.fromBytes(value));
   }
 
   get timestamp(): BigInt {
--- src/mapping.ts
+++ src/mapping.ts
@@ -3,81 +3,72 @@ import { Contract, NewTellorAddress } from "../generated/Contract/Contract";
 import {
   DisputeContract,
   NewDispute,
+  Voted,
+  DisputeVoteTallied,
 } from "../generated/Dispute/DisputeContract";
-import { Dispute } from "../generated/schema";
+import {
+  LibraryContract,
+  DataRequested,
+  NewChallenge,
+  NewValue,
+  NonceSubmitted,
+} from "../generated/Library/LibraryContract";
+import {
+  Dispute,
+  Vote,
+  Request,
+  MiningEvent,
+  MinerValue,
+} from "../generated/schema";
 
-export function handleNewTellorAddress(event: NewTellorAddress): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  // let entity = ExampleEntity.load(event.transaction.from.toHex());
-  // // Entities only exist after they have been saved to the store;
-  // // `null` checks allow to create entities on demand
-  // if (entity == null) {
-  //   entity = new ExampleEntity(event.transaction.from.toHex());
-  //   // Entity fields can be set using simple assignments
-  //   entity.count = BigInt.fromI32(0);
-  // }
-  // // BigInt and BigDecimal math are supported
-  // entity.count = entity.count + BigInt.fromI32(1);
-  // // Entity fields can be set based on event parameters
-  // entity._newTellor = event.params._newTellor;
-  // // Entities can be written to the store with `.save()`
-  // entity.save();
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.getRequestIdByTimestamp(...)
-  // - contract.getSubmissionsByTimestamp(...)
-  // - contract.getAddressVars(...)
-  // - contract.getSymbol(...)
-  // - contract.getName(...)
-  // - contract.totalSupply(...)
-  // - contract.getVariablesOnDeck(...)
-  // - contract.getRequestIdByQueryHash(...)
-  // - contract.getLastNewValueById(...)
-  // - contract.isInDispute(...)
-  // - contract.getNewValueCountbyRequestId(...)
-  // - contract.balanceOfAt(...)
-  // - contract.getUintVar(...)
-  // - contract.getRequestIdByRequestQIndex(...)
-  // - contract.didMine(...)
-  // - contract.getMinersByRequestIdAndTimestamp(...)
-  // - contract.balanceOf(...)
-  // - contract.getStakerInfo(...)
-  // - contract.getTimestampbyRequestIDandIndex(...)
-  // - contract.getDisputeUintVars(...)
-  // - contract.retrieveData(...)
-  // - contract.allowedToTrade(...)
-  // - contract.getCurrentVariables(...)
-  // - contract.didVote(...)
-  // - contract.getAllDisputeVars(...)
-  // - contract.getRequestQ(...)
-  // - contract.getMinedBlockNum(...)
-  // - contract.getDisputeIdByDisputeHash(...)
-  // - contract.allowance(...)
-  // - contract.getRequestUintVars(...)
-  // - contract.getRequestVars(...)
-  // - contract.getLastNewValue(...)
+export function handleDataRequested(event: DataRequested): void {
+  let request = new Request(event.params._requestId.toString());
+  request.requestId = event.params._requestId;
+  request.query = event.params._query;
+  request.querySymbol = event.params._querySymbol;
+  request.granularity = event.params._granularity;
+  request.totalTips = event.params._totalTips;
+  request.sender = event.params._sender;
+  request.timestamp = event.block.timestamp;
+
+  request.save();
+}
+
+// event NewValue(uint256 indexed _requestId, uint256 _time, uint256 _value, uint256 _totalTips, bytes32 _currentChallenge);
+export function handleNewValue(event: NewValue): void {
+  let miningEvent = new MiningEvent(
+    event.params._currentChallenge.toHexString()
+  );
+  miningEvent.timestamp = event.block.timestamp;
+  miningEvent.requestId = event.params._requestId;
+  miningEvent.request = event.params._requestId.toString();
+  miningEvent.time = event.params._time;
+  miningEvent.minedValue = event.params._value;
+  miningEvent.totalTips = event.params._totalTips;
+  miningEvent.currentChallenge = event.params._currentChallenge;
+
+  miningEvent.save();
+}
+
+// event NonceSubmitted(address indexed _miner, string _nonce, uint256 indexed _requestId, uint256 _value, bytes32 _currentChallenge);
+export function handleNonceSubmitted(event: NonceSubmitted): void {
+  let valueId = event.params._currentChallenge
+    .toHexString()
+    .concat("-value-")
+    .concat(event.params._miner.toHexString());
+  let value = new MinerValue(valueId);
+  value.timestamp = event.block.timestamp;
+  value.requestId = event.params._requestId;
+  value.currentChallenge = event.params._currentChallenge;
+  value.miningEvent = event.params._currentChallenge.toHexString();
+  value.miner = event.params._miner;
+  value.nonce = event.params._nonce;
+  value.value = event.params._value;
+  value.save();
 }
 
 export function handleNewDispute(event: NewDispute): void {
-  log.info("**** newDispute disputeId, {}", [
-    event.params._disputeId.toString(),
-  ]);
-
   let dispute = new Dispute(event.params._disputeId.toString());
-  dispute.count = BigInt.fromI32(0);
   dispute.disputeId = event.params._disputeId;
   dispute.miner = event.params._miner;
   dispute.requestId = event.params._requestId;
@@ -85,3 +76,68 @@ export function handleNewDispute(event: NewDispute): void {
 
   dispute.save();
 }
+
+// event Voted(uint256 indexed _disputeID, bool _position, address indexed _voter);
+export function handleVoted(event: Voted): void {
+  let voteId = event.params._disputeID
+    .toString()
+    .concat("-vote-")
+    .concat(event.params._voter.toHex());
+  let vote = new Vote(voteId);
+
+  vote.dispute = event.params._disputeID.toString();
+  vote.disputeId = event.params._disputeID;
+  vote.position = event.params._position;
+  vote.voter = event.params._voter;
+  vote.timestamp = event.block.timestamp;
+
+  vote.save();
+}
+
+// event DisputeVoteTallied(uint256 indexed _disputeID, int256 _result, address indexed _reportedMiner, address _reportingParty, bool _active);
+export function handleDisputeVoteTallied(event: DisputeVoteTallied): void {
+  let dispute = Dispute.load(event.params._disputeID.toString());
+
+  dispute.result = event.params._result;
+  dispute.reportedMiner = event.params._reportedMiner;
+  dispute.reportingParty = event.params._reportingParty;
+  dispute.active = event.params._active;
+
+  dispute.save();
+}
+
+export function handleNewTellorAddress(event: NewTellorAddress): void {}
+
+// contract methods
+// - contract.getRequestIdByTimestamp(...)
+// - contract.getSubmissionsByTimestamp(...)
+// - contract.getAddressVars(...)
+// - contract.getSymbol(...)
+// - contract.getName(...)
+// - contract.totalSupply(...)
+// - contract.getVariablesOnDeck(...)
+// - contract.getRequestIdByQueryHash(...)
+// - contract.getLastNewValueById(...)
+// - contract.isInDispute(...)
+// - contract.getNewValueCountbyRequestId(...)
+// - contract.balanceOfAt(...)
+// - contract.getUintVar(...)
+// - contract.getRequestIdByRequestQIndex(...)
+// - contract.didMine(...)
+// - contract.getMinersByRequestIdAndTimestamp(...)
+// - contract.balanceOf(...)
+// - contract.getStakerInfo(...)
+// - contract.getTimestampbyRequestIDandIndex(...)
+// - contract.getDisputeUintVars(...)
+// - contract.retrieveData(...)
+// - contract.allowedToTrade(...)
+// - contract.getCurrentVariables(...)
+// - contract.didVote(...)
+// - contract.getAllDisputeVars(...)
+// - contract.getRequestQ(...)
+// - contract.getMinedBlockNum(...)
+// - contract.getDisputeIdByDisputeHash(...)
+// - contract.allowance(...)
+// - contract.getRequestUintVars(...)
+// - contract.getRequestVars(...)
+// - contract.getLastNewValue(...)
